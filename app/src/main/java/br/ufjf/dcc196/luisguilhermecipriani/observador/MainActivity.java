992754b5bcc8d4972bf0aecabb1b6ce75a95c377
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-24 09:16:46 -0300

    Adiciona recyclerViewAvistamentos

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/observador/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/observador/MainActivity.java
@@ -1,14 +1,18 @@
 package br.ufjf.dcc196.luisguilhermecipriani.observador;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerViewAvistamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewAvistamentos = findViewById(R.id.recyclerViewAvistamentos);
     }
 }
