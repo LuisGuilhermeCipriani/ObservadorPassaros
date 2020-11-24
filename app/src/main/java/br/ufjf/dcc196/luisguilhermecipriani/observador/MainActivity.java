7fc4936d8d0b4fc8b273dbbb041f28018fae4006
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-24 09:30:40 -0300

    Adiciona lista de avistamentos

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/observador/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/observador/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerViewAvistamentos;
+    List<Avistamento> avistamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        avistamentos = new ArrayList<Avistamento>(){{
+            add(new Avistamento("Bem-te-vi", "Pitangus sulphuratus", 0));
+            add(new Avistamento("Martinho Pescador", "Megaceryle torquata", 1));
+            add(new Avistamento("João de Barro", "Furnarius rufus", 3));
+        }};
 
         recyclerViewAvistamentos = findViewById(R.id.recyclerViewAvistamentos);
     }
