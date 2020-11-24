f0264c44eacd3785a95c32959c54e6352cf96188
Luis Guilherme Cipriani <lgccipriani1@ice.ufjf.br>
2020-11-24 10:37:59 -0300

    Altera adapter para mostrar os dados

--- app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/observador/AvistamentoAdapter.java
+++ app/src/main/java/br/ufjf/dcc196/luisguilhermecipriani/observador/AvistamentoAdapter.java
@@ -1,7 +1,10 @@
 package br.ufjf.dcc196.luisguilhermecipriani.observador;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,22 +22,34 @@ public class AvistamentoAdapter extends RecyclerView.Adapter<AvistamentoAdapter.
     @NonNull
     @Override
     public AvistamentoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context ctx = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View avistamentoView = inflater.inflate(R.layout.avistamento_layout, parent, false);
+        AvistamentoViewHolder viewHolder = new AvistamentoViewHolder(avistamentoView);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull AvistamentoViewHolder holder, int position) {
-
+        holder.textViewNome.setText(avistamentos.get(position).getNome());
+        holder.textViewEspecie.setText(avistamentos.get(position).getEspecie());
+        holder.textViewAvistamentos.setText(avistamentos.get(position).getAvistamentos().toString());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return avistamentos.size();
     }
 
     public class AvistamentoViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewNome;
+        private TextView textViewEspecie;
+        private TextView textViewAvistamentos;
         public AvistamentoViewHolder(@NonNull View itemView) {
             super(itemView);
+            textViewNome = itemView.findViewById(R.id.textViewNome);
+            textViewEspecie = itemView.findViewById(R.id.textViewEspecie);
+            textViewAvistamentos= itemView.findViewById(R.id.textViewAvistamentos);
         }
     }
 }
